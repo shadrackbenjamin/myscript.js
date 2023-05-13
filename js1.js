5bc9a485ed2a3d688f65d5ba795c8e0d4109d18d
shadrackbenjamin <shadrackbenjamin99@gmail.com>
2023-05-13 16:30:54 +0000

    first week javascript

---
+++ js1.js
@@ -0,0 +1,16 @@
+var name = "baaba"
+var age = "12"
+var residence = "ashtown"
+var location = 'kumasi'
+
+
+console.log(name);
+console.log(age);
+console.log(residence)
+console.log(location)
+
+let surNmae = (maame)
+let age = (70)
+let address = (GA)
+let school = (codetrain)
+let movies = (antman)
